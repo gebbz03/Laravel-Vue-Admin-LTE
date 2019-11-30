0ddb0db3e624d782bbb61a6b0256213227d936bf
unknown <gebb.freelancer@gmail.com>
2019-11-30 10:09:29 +0800

    v6

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,26 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
 
 import { Form, HasError, AlertError } from 'vform';
+
+import Swal from 'sweetalert2'
+window.Swal=Swal;
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  window.Toast=Toast;
 
 window.Form=Form;
 Vue.component(HasError.name, HasError)
@@ -16,6 +34,24 @@ Vue.component(AlertError.name, AlertError)
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+
+const options = {
+    color: '#bffaf3',
+    failedColor: '#874b4b',
+    thickness: '5px',
+    transition: {
+      speed: '0.2s',
+      opacity: '0.6s',
+      termination: 300
+    },
+    autoRevert: true,
+    location: 'left',
+    inverse: false
+  }
+
+  Vue.use(VueProgressBar, options)
 
 
 let routes = [
@@ -28,6 +64,23 @@ const router = new VueRouter({
     mode: 'history',
     routes // short for `routes: routes`
 })
+
+
+// Vue.filter('upText', function(text){
+//     return text.toUpperCase();
+// });
+
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate',function(createddate){
+    return moment(createddate).format('MMMM Do YYYY, h:mm:ss a');
+});
+
+
+let Fire=new Vue();
+window.Fire=Fire;
 
 /**
  * The following block of code may be used to automatically register your
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -27,14 +27,12 @@
                 </tr>
               </thead>
               <tbody>
-
-
                 <tr v-for="user in users" :key="user.id">
-                  <td> {{user.id}} </td>
-                  <td> {{user.name}} </td>
-                  <td> {{user.email}} </td>
-                  <td> {{user.type}} </td>
-                  <td> {{user.created_at}} </td>
+                  <td>{{user.id}}</td>
+                  <td>{{user.name}}</td>
+                  <td>{{user.email}}</td>
+                  <td>{{user.type | upText}}</td>
+                  <td>{{user.created_at | myDate}}</td>
                   <td>
                     <a href>
                       <i class="fa fa-edit orange"></i>
@@ -45,7 +43,6 @@
                     </a>
                   </td>
                 </tr>
-
               </tbody>
             </table>
           </div>
@@ -74,89 +71,77 @@
           </div>
 
           <form @submit.prevent="createUser" @keydown="form.onKeydown($event)">
-          <div class="modal-body">
+            <div class="modal-body">
+              <div class="form-group">
+                <input
+                  v-model="form.name"
+                  type="text"
+                  name="name"
+                  placeholder="Enter you Name.."
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('name') }"
+                />
+                <has-error :form="form" field="name"></has-error>
+              </div>
 
-            <div class="form-group">
-              <input
-                v-model="form.name"
-                type="text"
-                name="name"
-                placeholder="Enter you Name.."
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('name') }"
-              />
-              <has-error :form="form" field="name"></has-error>
+              <div class="form-group">
+                <input
+                  v-model="form.email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter you Email.."
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('email') }"
+                />
+                <has-error :form="form" field="email"></has-error>
+              </div>
+
+              <div class="form-group">
+                <textarea
+                  v-model="form.bio"
+                  name="bio"
+                  id="bio"
+                  placeholder="Biography for User(Optional).."
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('bio') }"
+                ></textarea>
+                <has-error :form="form" field="bio"></has-error>
+              </div>
+
+              <div class="form-group">
+                <p>Select User Role</p>
+                <select
+                  name="type"
+                  id="type"
+                  v-model="form.type"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('type') }"
+                >
+                  <option value="admin">Admin</option>
+                  <option value="user">Standard User</option>
+                  <option value="author">Author</option>
+                </select>
+                <has-error :form="form" field="type"></has-error>
+              </div>
+
+              <div class="form-group">
+                <input
+                  v-model="form.password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter you Password.."
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('password') }"
+                />
+                <has-error :form="form" field="password"></has-error>
+              </div>
             </div>
 
-            <div class="form-group">
-              <input
-                v-model="form.email"
-                type="email"
-                name="email"
-                placeholder="Enter you Email.."
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('email') }"
-              />
-              <has-error :form="form" field="email"></has-error>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Create</button>
             </div>
-
-
-            <div class="form-group">
-              <textarea
-                v-model="form.bio"
-                name="bio"
-                id="bio"
-                placeholder="Biography for User(Optional).."
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('bio') }"
-              ></textarea>
-              <has-error :form="form" field="bio"></has-error>
-            </div>
-
-
-            <div class="form-group">
-              <p>Select User Role</p>
-              <select
-                name="type"
-                id="type"
-                v-model="form.type"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('type') }"
-              >
-                <option value="admin">Admin</option>
-                <option value="user">Standard User</option>
-                <option value="author">Author</option>
-              </select>
-              <has-error :form="form" field="type"></has-error>
-            </div>
-
-
-
-            <div class="form-group">
-              <input
-                v-model="form.password"
-                type="password"
-                name="password"
-                placeholder="Enter you Password.."
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('password') }"
-              />
-              <has-error :form="form" field="password"></has-error>
-            </div>
-
-
-          </div>
-     
-
-
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Create</button>
-          </div>
-
-         </form>
-
-
+          </form>
         </div>
       </div>
     </div>
@@ -167,31 +152,42 @@
 export default {
   data() {
     return {
-      
       // Create a new form instance\
-      users:{},
+      users: {},
       form: new Form({
-        name: '',
-        email: '',
-        password: '',
-        type: '',
-        bio: '',
-        photo: ''
+        name: "",
+        email: "",
+        password: "",
+        type: "",
+        bio: "",
+        photo: ""
       })
     };
   },
 
-  methods:{
-    loadUsers(){
-      axios.get("api/user").then(({data}) => (this.users=data.data));
+  methods: {
+    loadUsers() {
+      axios.get("api/user").then(({ data }) => (this.users = data.data));
     },
-    createUser(){
-      this.form.post('api/user');
+    createUser() {
+      this.$Progress.start();
+      this.form.post("api/user");
+      Fire.$emit('AfterInserted');
+      $('#newUserModal').modal('hide');
+      Toast.fire({
+        icon: "success",
+        title: "Data successfully inserted!"
+      });
+      this.$Progress.finish();
     }
   },
 
   created() {
     this.loadUsers();
+    Fire.$on('AfterInserted',() =>{
+      this.loadUsers();
+    })
+    // setInterval(() => this.loadUsers(),3000);
   }
 };
 </script>
